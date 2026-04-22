ca80b37b47156e6df5cda194902bcfeae61fd528
ankhtn <anndhd18@gmail.com>
2026-04-22 16:48:54 +0700

    Đổi lại các button team [N]

--- script.js
+++ script.js
@@ -457,6 +457,9 @@ function updateTeamButtonsUI() {
     if (completedTeams.includes(i)) {
       btn.classList.add('team-completed');
       btn.disabled = true;
+    } else if (activeTeam === i) {
+      btn.classList.add('team-active');
+      btn.disabled = false;
     } else {
       let isBtnDisabled = !isBoxActive;
       btn.disabled = isBtnDisabled;
