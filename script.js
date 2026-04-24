ff627f8699a91d6025b950483cb3a8df43f812a1
ankhtn <anndhd18@gmail.com>
2026-04-24 11:20:09 +0700

    update Full Screen button

--- script.js
+++ script.js
@@ -1259,11 +1259,14 @@ window.onload = () => {
 
 document.addEventListener("fullscreenchange", () => {
   const btn = document.getElementById("btn-fullscreen");
+  const iconEnter = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/></svg>`;
+  const iconExit = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"/></svg>`;
+  
   if (document.fullscreenElement) {
-    if (btn) btn.innerText = "🗗";
+    if (btn) btn.innerHTML = iconExit;
     document.body.classList.add("is-fullscreen");
   } else {
-    if (btn) btn.innerText = "⛶";
+    if (btn) btn.innerHTML = iconEnter;
     document.body.classList.remove("is-fullscreen");
   }
 });
